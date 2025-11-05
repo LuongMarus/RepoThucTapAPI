c6250461b2006d3851e1bca9dc4714242019fddb
thiendao2308 <thiendaocong@gmail.com>
2025-11-05 19:59:20 +0700

    create project

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+// Flat config for ESLint (ESM)
+import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**', 'coverage/**', 'eslint.config.cjs']
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'commonjs',
+      parser: tsParser,
+      globals: {
+        console: 'readonly',
+        process: 'readonly'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin
+    },
+    rules: {
+      'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }]
+    }
+  }
+];
+
+
