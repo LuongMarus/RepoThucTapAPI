ab9f1d36a747d488f69a24785f42263daee12971
quan3473 <nguyenminhquan042004@gmail.com>
2025-11-09 13:33:34 +0700

    Enhance User model by adding required password and optional secretOtp fields; introduce KeyToken model for managing user tokens with associated fields and relationships; update Express types to include refresh and temp token payloads.

--- src/common/global/express.d.ts
+++ src/common/global/express.d.ts
@@ -1,5 +1,7 @@
 import 'express';
 import { RoleType, RoleScope } from '@/types/role';
+import { RefreshTokenPayload } from '@/types/jwt';
+import { TempTokenPayload } from '@/modules/identity/key-token/key-token.service';
 
 declare global {
   namespace Express {
@@ -12,9 +14,23 @@ declare global {
         roleScope: RoleScope;
         permissions: string[];
       };
+      refresh: RefreshTokenPayload;
+      refreshToken?: string;
+
+      tempTokenPayload?: TempTokenPayload;
+
       resource?: any; // For resource ownership validation
       changeReason?: string; // For audit trail
       auditAction?: string; // For audit logging
+
+      apiKey: {
+        keyId: string;
+        owner: string;
+        scopes: string[];
+      };
+
+      // For rate limit
+      rateLimitKey?: string;
 
       // For file
       file?: Express.Multer.File;
